90afb5e4f5d90461e3c6252b1e2b3a7ee41e224b
Albert Minnie <albert.minnie@gmail.com>
2021-10-06 08:54:59 +0200

    add manager field to client model

--- clientmanager/models.py
+++ clientmanager/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Client(models.Model):
+    manager = models.ForeignKey(User, on_delete=models.CASCADE)
     email = models.CharField(max_length=200)
 
 class Document(models.Model):
