df265cd3af5c61a1e233ac7dbcf34cffee54f8f3
Albert Minnie <albert.minnie@gmail.com>
2021-10-06 08:41:44 +0200

    add client and document models for app

--- clientmanager/models.py
+++ clientmanager/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    email = models.CharField(max_length=200)
+
+class Document(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'clientmanager.apps.ClientmanagerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
